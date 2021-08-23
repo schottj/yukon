f6f426bee5ca352eb820023b3a82a8dcb2324baf
Julia Schottenstein <julia.schottenstein@dbtlabs.com>
2021-08-23 20:53:30 +0000

    removed default value for key

--- models/env_vars.sql
+++ models/env_vars.sql
@@ -1,6 +1,6 @@
 with env_var as (
     select 
-        '{{env_var("KEY","CODE")}}' as test_ev,
+        '{{env_var("KEY")}}' as test_ev,
         '{{env_var("DBT_CLOUD_RUN_ID", "DEV")}}' as run_id,
         '{{env_var("DBT_ENV_SECRET_GIT_TOKEN", "TOKEN")}}' as token
     from dual
