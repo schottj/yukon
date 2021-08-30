cdcb18eb5bc106bcfac9e981f0a93f9facdde809
Julia Schottenstein <julia.schottenstein@dbtlabs.com>
2021-08-30 23:42:14 +0000

    update

--- models/env_vars.sql
+++ models/env_vars.sql
@@ -2,7 +2,8 @@ with env_var as (
     select 
         '{{env_var("ENVIRONMENT")}}' as test_ev,
         '{{env_var("DBT_CLOUD_RUN_ID", "DEV")}}' as run_id
-    from dual
+    from {{ref('orders')}}
+    where order_id = 86
 )
 
 Select * from env_var
