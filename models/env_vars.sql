6726e2760927a6abf93fbb2119f8b50f5124c7f3
Julia Schottenstein <julia.schottenstein@dbtlabs.com>
2021-09-17 18:36:43 +0000

    case sensitive

--- models/env_vars.sql
+++ models/env_vars.sql
@@ -1,7 +1,7 @@
 with env_var as (
     select 
         '{{env_var("ENVIRONMENT")}}' as test_ev,
-        '{{env_var("DBT_CLOUD_RUN_ID", "DEV")}}' as run_id
+        '{{env_var("DBT_cloud_run_id", "DEV")}}' as run_id
     from {{ref('orders')}}
     where order_id = 86
 )
